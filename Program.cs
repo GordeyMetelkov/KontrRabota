bcbc234b27fec5b0e6cb9c06fb3d7db833205b79
Gordey <gordeymetelkov@icloud.com>
2022-08-02 21:28:22 +0300

    добавил метод заполнения первоначального массива

--- Program.cs
+++ Program.cs
@@ -1,2 +1,10 @@
 ﻿int size = 3;
 string [] massiv = new String [size];
+void FillArray (string [] currentArray)
+{
+    for (int index = 0; index < currentArray.Length; index++)
+    {
+        Console.WriteLine("Введите элемент массива: ");
+        currentArray[index] = Console.ReadLine();
+    }
+}
